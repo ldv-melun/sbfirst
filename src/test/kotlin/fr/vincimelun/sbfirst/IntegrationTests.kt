c9ab31c70a794d8060a959602b780b5231f8cc79
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2022-10-17 14:18:57 +0200

    update README.md and add test

---
+++ src/test/kotlin/fr/vincimelun/sbfirst/IntegrationTests.kt
@@ -0,0 +1,22 @@
+package fr.vincimelun.sbfirst
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests constructor(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert hello page title, content and status code`() {
+        val httpEntity = restTemplate.getForEntity<String>("/")
+        assertEquals(httpEntity.statusCode, HttpStatus.OK)
+        assertNotNull(httpEntity.body)
+        assertTrue(httpEntity.body!!.contains("<h1>Hello world !</h1>"))
+    }
+
+}
